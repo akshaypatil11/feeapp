447570ef3d6984510bd152e0f204c51f55d37865
akshay <ak.patil3034@gmail.com>
2025-07-15 12:50:26 +0530

    Initial commit

---
+++ src/main/java/com/feeapp/feeapp/FeeappApplication.java
@@ -0,0 +1,13 @@
+package com.feeapp.feeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeappApplication.class, args);
+	}
+
+}
